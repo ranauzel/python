92e13511d0cd436cb9738492692a8d54e4a4ff1d
Sümeyye Rana <74724438+ranauzel@users.noreply.github.com>
2022-10-19 19:45:21 +0300

    Add files via upload

---
+++ 2_hafta/17_operatorler.py
@@ -0,0 +1,10 @@
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+
+print(9 % 2)
+'''kalan'''
+print(9 // 2)
+'bölüm'
+print(3 ** 5)
---
+++ 2_hafta/19_listeler_2.py
@@ -0,0 +1,20 @@
+a = [1, 6]
+a.append(5)
+print(a)
+
+a.pop()
+print(a)
+
+a.append(5)
+a.remove(1)
+print(a)
+
+a.insert(0,100)
+print(a)
+
+
+print(a.pop())
+print(a)
+
+a.clear()
+print(a)
